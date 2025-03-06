d22407665f503e3bab7e7e0d56fcdaf6d9ccce68
victor <silvamatheus903@gmail.com>
2025-03-06 07:28:06 -0300

    att

---
+++ bind.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DNS_DIR="./sprint1/dns"
+cd "$DNS_DIR" || exit
+
+# Definindo os domínios e o IP
+zones=("areiabranca.com.br" "golfinhos.com.br" "madeiro.com.br" "pontanegra.com.br" "praiadoamor.com.br" "praiadoforte.com.br" "praiadomeio.com.br")
+ip=$(netstat -rn | grep "0.0.0.0" | awk 'NR==1 {print $4}')
+
+# Loop para criar os arquivos de zona para cada domínio
+for dom in "${zones[@]}"; do
+    # Criando o conteúdo do arquivo de zona
+    body="$TTL 86400
+@   IN  SOA ns1.$dom. admin.$dom. (
+        2025022003  
+        3600        
+        1800        
+        1209600     
+        86400 )     
+
+    IN  NS  ns1.$dom.
+    IN  NS  ns2.$dom.
+@   IN  A   $ip
+ns1 IN  A   $ip
+ns2 IN  A   $ip
+www IN  A   $ip
+
+@   IN  MX  10 mail.$dom.
+mail IN  A   $ip
+"
+
+    # Nome do arquivo a ser criado
+    arquivo="$dom.zone"
+
+    # Escrevendo o conteúdo no arquivo
+    echo "$body" > "$arquivo"
+    echo "Arquivo de zona para $dom criado com sucesso: $arquivo"
+done
